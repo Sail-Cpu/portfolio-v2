b7d07386da2bce9399dc5bbc877ce6ecf66e4f11
sofiane lasoa <sofiane.lasoa.etu@univ-lille.fr>
2023-04-25 01:29:41 +0200

    Implement Picture

--- src/pages/section/Me.js
+++ src/pages/section/Me.js
@@ -1,9 +1,12 @@
 import React from "react";
 import styled from "styled-components";
+import theme from "../../styles/Theme";
 //Components
 import SectionName from "../../components/SectionName";
+import Picture from "../../assets/img/picture.png";
 
 const StyledMe = styled.section`
+  position: relative;
   margin-top: 150px;
   width: 100%;
   height: 100vh;
@@ -11,10 +14,53 @@ const StyledMe = styled.section`
   max-height: 820px;
 `;
 
+const StyledMeBar = styled.div`
+  position: absolute;
+  top: 45px;
+  left: ${(props) => (props.left ? "0" : "")};
+  right: ${(props) => (props.right ? "0" : "")};
+  width: ${(props) => props.width + "%"};
+  height: 2px;
+  background-color: ${theme.colors.fourth};
+`;
+
+const StyledPictureContainer = styled.div`
+  position: absolute;
+  top: 45px;
+  left: 60%;
+  width: 25%;
+  height: 260px;
+  border: 3px solid ${theme.colors.secondary};
+  .picture {
+    position: absolute;
+    top: 15px;
+    right: -15px;
+    width: 100%;
+    height: 100%;
+    background-image: url("${Picture}");
+    background-size: cover;
+    background-position: center;
+  }
+  div {
+    position: absolute;
+    top: 15px;
+    right: -15px;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(38, 49, 89, 0.2);
+  }
+`;
+
 const Me = () => {
   return (
     <StyledMe>
       <SectionName nb="01" name="Moi" />
+      <StyledMeBar width="55" left />
+      <StyledPictureContainer>
+        <div className="picture"></div>
+        <div></div>
+      </StyledPictureContainer>
+      <StyledMeBar width="10" right />
     </StyledMe>
   );
 };
--- src/styles/Theme.js
+++ src/styles/Theme.js
@@ -2,7 +2,8 @@ const theme = {
   colors: {
     primary: "#263159",
     secondary: "#FFFBEB",
-    third: "#00FFD1"
+    third: "#00FFD1",
+    fourth: "#566285",
   },
 };
 
