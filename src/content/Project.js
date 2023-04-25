6cb025b53c410e5d55e287959e817ee8d0c0fff0
sofiane lasoa <sofiane.lasoa.etu@univ-lille.fr>
2023-04-25 23:47:31 +0200

    Implement Project Slider

---
+++ src/content/Project.js
@@ -0,0 +1,40 @@
+const Project = [
+  {
+    name: "Alcorn ' List",
+    description:
+      "Un catalogue de jeux vidéo présentant \n" +
+      "plus de 500 000 jeux, triés dans différentes \n" +
+      "catégories.",
+  },
+  {
+    name: "OUT.",
+    description:
+      "Une application qui te donne des conseils sur les vêtements " +
+      "à porter en fonction de la météo, tu choisis la localisation et " +
+      "elle te donne une liste de pantalons, de vestes, etc. que tu peux porter.",
+  },
+  {
+    name: "Dragon.io",
+    description:
+      "Une adaptation d'Agar.io basée sur l'univers de Dragon Ball Z. Le jeu " +
+      "met en scène les personnages emblématiques de la série, tels que Goku, Vegeta " +
+      "ou Freezer, et vous permet d'absorber des boules de ki pour devenir plus fort et " +
+      "combattre d'autres joueurs en ligne.",
+  },
+  {
+    name: "Sera",
+    description:
+      "Une API de manga qui permet d'accéder à une vaste sélection de titres populaires. " +
+      "Que vous soyez amateur de manga ou développeur d'application, SERA est facile à " +
+      "utiliser et vous permettra d'intégrer facilement des informations sur les mangas " +
+      "dans vos projets.",
+  },
+  {
+    name: "Portfolio v1",
+    description:
+        "Mon Cv numérique qui vous parle de moi, de mes compétences, " +
+        "de mes projets et qui vous donnera je l'espère l'envie de travailler avec moi.",
+  },
+];
+
+export default Project;
--- src/pages/section/Project.js
+++ src/pages/section/Project.js
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import SectionName from "../../components/SectionName";
 import theme from "../../styles/Theme";
+//Project
+import AllProject from "../../content/Project";
 
 const StyledProject = styled.div`
   position: relative;
   top: 650px;
   width: 100%;
+  .slider-container {
+    width: 91%;
+    margin: 0 auto;
+  }
 `;
-
 const StyledProjectTop = styled.div`
   display: flex;
   align-items: center;
   width: 100%;
+  margin-bottom: 25px;
   .project-bar-left {
     margin-left: 20px;
     width: 20%;
@@ -27,8 +33,35 @@ const StyledProjectTop = styled.div`
     background-color: ${theme.colors.fourth};
   }
 `;
+const StyledSlider = styled.div`
+  display: flex;
+  flex-direction: column;
+  .slider-little-bar {
+    width: 4px;
+    height: 30px;
+    background-color: ${theme.colors.secondary};
+  }
+`;
+const StyledSlide = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 200px;
+  height: 70px;
+  font-size: 25px;
+  border-left: ${(props) =>
+    props.active
+      ? "4px solid" + theme.colors.third
+      : "4px solid" + theme.colors.secondary};
+  border-right: ${(props) =>
+    props.active ? "4px solid" + theme.colors.third : ""};
+  box-shadow: ${(props) =>
+    props.active ? "0px 6px 15px rgba(0, 0, 0, 0.25)" : ""};
+`;
 
 const Project = () => {
+  const [activeProject, setActiveProject] = useState(0);
+
   return (
     <StyledProject>
       <StyledProjectTop>
@@ -36,6 +69,21 @@ const Project = () => {
         <div className="project-bar-left"></div>
         <div className="project-bar-right"></div>
       </StyledProjectTop>
+      <div className="slider-container">
+        <StyledSlider>
+          <div className="slider-little-bar"></div>
+          {AllProject.map((project, idx) => {
+            return (
+              <StyledSlide
+                onClick={() => setActiveProject(idx)}
+                active={activeProject === idx}
+              >
+                {project.name}
+              </StyledSlide>
+            );
+          })}
+        </StyledSlider>
+      </div>
     </StyledProject>
   );
 };
